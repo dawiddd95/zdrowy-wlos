442ac78d6d2d8e33d3f5f5bd9cc1126597d349aa
Dawid <42100660+dawiddd95@users.noreply.github.com>
2018-10-13 14:26:02 +0200

    Update fullscreen-navigation.js

--- scripts/fullscreen-navigation.js
+++ scripts/fullscreen-navigation.js
@@ -1,29 +1,18 @@
-/*let openButton = document.querySelector(".open-button");
-let closeButton = document.querySelector(".close-button");
-let overlay = document.querySelector(".overlay");
-let body = document.querySelector(".body");
-
-openButton.addEventListener("click", openNav);
-closeButton.addEventListener("click", closeNav);
-
-function openNav() {
-    overlay.style.width = "90%";
-    body.style.overflowY = "hidden";
-}
-
-function closeNav() {
-    overlay.style.width = "0%";
-    body.style.overflowY = "scroll";
-}
-*/
-
 
 function init() {
 	let acc = document.getElementsByClassName("open-button");
+	let toggle = false;
 	for ( let i = 0; i < acc.length; i++) {
 		acc[i].addEventListener("click", showContent);
 	}
 	function showContent() {
+	if (toggle == true) {
+		document.querySelector('.menu').src = 'images/menu-options.png';
+	} else {
+		document.querySelector('.menu').src = 'images/close.png';
+	}
+	toggle != toggle;	
+		
         this.nextElementSibling.classList.toggle("open-overlay");
 	}
 }
